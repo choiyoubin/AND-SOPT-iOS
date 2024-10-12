d71fbc186eb000da3a5250fe2017ded3024292c9
최유빈 <cub8533@naver.com>
2024-10-12 15:10:26 +0900

    week/2 - [Feat] nickName label, delegate 구현

--- SOPT-35-Seminar/SOPT-35-Seminar/DetailViewController.swift
+++ SOPT-35-Seminar/SOPT-35-Seminar/DetailViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    weak var delegate: NicknameDelegate?
+    
     private let titleLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 16, weight: .semibold)
@@ -36,20 +38,37 @@ class DetailViewController: UIViewController {
         return button
     }()
     
+    private let nickNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "닉네임을 입력해주세요."
+        textField.clearButtonMode = .whileEditing
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.layer.borderWidth = 1
+        // 추가한 부분
+        textField.textColor = .white
+        return textField
+    }()
+    
+//    private lazy var settingButton: UIButton = {
+//        let button = UIButton()
+//        button.setTitle("닉네임 설정", for: .normal)
+//        button.backgroundColor = .tintColor
+//        button.setTitleColor(.white, for: .normal)
+//        button.addTarget(self, action: #selector(settingButtonTapped), for: .touchUpInside)
+//
+//        return button
+//    }()
+    
     private var receivedTitle: String?
     private var receivedContent: String?
     
     @objc func backButtonTapped() {
-        goBack()
+        if let nickname = nickNameTextField.text {
+            delegate?.dataBind(nickname: nickname)
+        }
+        self.navigationController?.popViewController(animated: true)
     }
     
-    private func goBack() {
-        if self.navigationController == nil {
-            self.dismiss(animated: true)
-        } else {
-            self.navigationController?.popViewController(animated: true)
-        }
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         setStyle()
@@ -58,12 +77,12 @@ class DetailViewController: UIViewController {
     }
     
     private func setStyle() {
-        self.view.backgroundColor = .systemPink
+        self.view.backgroundColor = .brown
     }
     
     private func setUI() {
         // UIView+ 적용
-        view.addSubviews(titleLabel, contentLabel, backButton)
+        view.addSubviews(titleLabel, contentLabel, backButton, nickNameTextField)
     }
     
     func updateUI() {
@@ -87,7 +106,7 @@ class DetailViewController: UIViewController {
         titleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -50).isActive = true
         titleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
-        contentLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200).isActive = true
+        contentLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 300).isActive = true
         contentLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 100).isActive = true
         contentLabel.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
@@ -95,6 +114,14 @@ class DetailViewController: UIViewController {
         backButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
         backButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         backButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -100).isActive = true
+        
+        nickNameTextField.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 200).isActive = true
+        nickNameTextField.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20).isActive = true
+        nickNameTextField.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20).isActive = true
+        nickNameTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
+}
 
+protocol NicknameDelegate: AnyObject {
+    func dataBind(nickname: String)
 }
