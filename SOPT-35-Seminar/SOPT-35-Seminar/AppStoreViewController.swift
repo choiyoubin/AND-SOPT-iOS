9d67656ce4d2a7267c3f65d4fdc6cc4765c6d94c
최유빈 <cub8533@naver.com>
2024-10-23 02:00:01 +0900

    week/2 - [Feat/Scene] Preview Extension 추가 및 평가, 수상, 연령 컴포넌트 구현

--- SOPT-35-Seminar/SOPT-35-Seminar/AppStoreViewController.swift
+++ SOPT-35-Seminar/SOPT-35-Seminar/AppStoreViewController.swift
@@ -17,6 +17,7 @@ class AppStoreViewController: UIViewController {
     }
     private let contentView = UIView()
     
+    // MARK: APP Details
     let IconImageView = UIImageView().then {
         $0.image = UIImage(named: "tossIcon")
     }
@@ -47,6 +48,72 @@ class AppStoreViewController: UIViewController {
         $0.tintColor = .tintColor
     }
     
+    // MARK: APP Description
+    private let ratingView = UIView()
+    
+    let rateTitleLabel = UILabel().then {
+        $0.text = "8.4만개의 평가"
+        $0.font = .systemFont(ofSize: 10)
+        $0.textAlignment = .center
+        $0.textColor = .gray
+    }
+    
+    let rateLabel = UILabel().then {
+        $0.text = "4.4"
+        $0.font = .systemFont(ofSize: 16, weight: .bold)
+        $0.textAlignment = .center
+        $0.textColor = .gray
+    }
+    
+    let rateStarView = UIImageView().then {
+        $0.image = UIImage(systemName: "star.fill")
+        $0.tintColor = .gray
+    }
+    
+    private let awardView = UIView()
+    
+    let awardTitleLabel = UILabel().then {
+        $0.text = "수상"
+        $0.font = .systemFont(ofSize: 10)
+        $0.textAlignment = .center
+        $0.textColor = .gray
+    }
+    
+    let awardImageview = UIImageView().then {
+        $0.image = UIImage(systemName: "person.fill")
+        $0.tintColor = .gray
+    }
+    
+    let awardCateLabel = UILabel().then {
+        $0.text = "앱"
+        $0.font = .systemFont(ofSize: 10)
+        $0.textAlignment = .center
+        $0.textColor = .gray
+    }
+    
+    private let ageView = UIView()
+    
+    let ageTitleLabel = UILabel().then {
+        $0.text = "연령"
+        $0.font = .systemFont(ofSize: 10)
+        $0.textAlignment = .center
+        $0.textColor = .gray
+    }
+    
+    let ageLabel = UILabel().then {
+        $0.text = "4+"
+        $0.font = .systemFont(ofSize: 16, weight: .bold)
+        $0.textAlignment = .center
+        $0.textColor = .gray
+    }
+    
+    let ageStandardLabel = UILabel().then {
+        $0.text = "세"
+        $0.font = .systemFont(ofSize: 10)
+        $0.textAlignment = .center
+        $0.textColor = .gray
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setStyle()
@@ -62,12 +129,15 @@ class AppStoreViewController: UIViewController {
     func setUI() {
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
-        contentView.addSubviews(IconImageView, titleLabel, subtitleLabel, openButton, shareButton)
+        ratingView.addSubviews(rateLabel, rateTitleLabel, rateStarView)
+        awardView.addSubviews(awardTitleLabel, awardImageview, awardCateLabel)
+        ageView.addSubviews(ageTitleLabel, ageLabel, ageStandardLabel)
+        contentView.addSubviews(IconImageView, titleLabel, subtitleLabel, openButton, shareButton, ratingView, awardView, ageView)
     }
     
     func setLayout() {
         scrollView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
             $0.width.equalToSuperview()
         }
         contentView.snp.makeConstraints {
@@ -99,5 +169,71 @@ class AppStoreViewController: UIViewController {
             $0.width.height.equalTo(20)
             $0.trailing.equalTo(contentView).offset(-20)
         }
+        
+        ratingView.snp.makeConstraints {
+            $0.top.equalTo(openButton.snp.bottom).offset(40)
+            $0.leading.equalTo(contentView).offset(20)
+            $0.width.equalTo((view.bounds.width - 40) * 0.33)
+            $0.height.equalTo(60)
+        }
+        rateTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(ratingView)
+            $0.centerX.equalTo(ratingView)
+        }
+        rateLabel.snp.makeConstraints {
+            $0.centerX.equalTo(rateTitleLabel)
+            $0.centerY.equalTo(ratingView)
+        }
+        // TODO: 평점 별 추가 (stackView)
+        rateStarView.snp.makeConstraints {
+            $0.centerX.equalTo(rateTitleLabel)
+            $0.bottom.equalTo(ratingView)
+            $0.width.height.equalTo(12)
+        }
+        
+        awardView.snp.makeConstraints {
+            $0.top.equalTo(ratingView)
+            $0.centerX.equalTo(contentView)
+            $0.width.equalTo((view.bounds.width - 40) * 0.33)
+            $0.height.equalTo(60)
+        }
+        awardTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(awardView)
+            $0.centerX.equalTo(awardView)
+        }
+        awardImageview.snp.makeConstraints {
+            $0.centerX.centerY.equalTo(awardView)
+            $0.width.height.equalTo(20)
+        }
+        awardCateLabel.snp.makeConstraints {
+            $0.centerX.equalTo(awardView)
+            $0.bottom.equalTo(awardView)
+        }
+        
+        ageView.snp.makeConstraints {
+            $0.top.equalTo(ratingView)
+            $0.trailing.equalTo(contentView).offset(-20)
+            $0.width.equalTo((view.bounds.width - 40) * 0.33)
+            $0.height.equalTo(60)
+        }
+        ageTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(ageView)
+            $0.centerX.equalTo(ageView)
+        }
+        ageLabel.snp.makeConstraints {
+            $0.centerX.equalTo(ageTitleLabel)
+            $0.centerY.equalTo(ageView)
+        }
+        ageStandardLabel.snp.makeConstraints {
+            $0.centerX.equalTo(ageTitleLabel)
+            $0.bottom.equalTo(ageView)
+        }
+        
+        
+        
     }
+}
+
+#Preview {
+    AppStoreViewController()
 }
